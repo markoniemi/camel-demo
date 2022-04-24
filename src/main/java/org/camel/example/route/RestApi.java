51a7fac9dffcd8eeaedff77bc6f44a96def131a6
Marko Niemi <marko.j.niemi@gmail.com>
2022-04-24 11:37:47 +0300

    use fuse bom

--- src/main/java/org/camel/example/route/RestApi.java
+++ src/main/java/org/camel/example/route/RestApi.java
@@ -9,7 +9,7 @@ class RestApi extends RouteBuilder {
     @Override
     public void configure() {
         getContext().setTracing(true);
-        restConfiguration().host("localhost").port(8080);
+        restConfiguration().port(8080);
         rest("/api/").id("api-route").get("/bean").produces("application/json").to("direct:remoteService");
         from("direct:remoteService").routeId("direct-route").log("${body}").transform().simple("Hello");
     }
--- src/test/java/org/camel/example/CamelDemoIT.java
+++ src/test/java/org/camel/example/CamelDemoIT.java
@@ -1,20 +1,29 @@
 package org.camel.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class CamelDemoIT {
     private TestRestTemplate testRestTemplate = new TestRestTemplate();
-    private String url="http://localhost:8080";
+    private String url = "http://localhost:8080";
+
     @Test
     void test() {
-        assertTrue(testRestTemplate.getForObject(url + "/api/bean", String.class).contains("Hello"));
+        ResponseEntity<String> response = testRestTemplate.exchange(url + "/api/bean", HttpMethod.GET, null,
+                String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        response.getBody();
+        assertTrue(response.getBody().contains("Hello"));
     }
 }
